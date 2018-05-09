71229bf02e5fa1fad136c5de3b3a9b96b087a815
Jay Hwang <jayhwang0121@gmail.com>
2018-05-09 11:18:26 -0700

    chore(db): add todo comment

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,12 @@
 const AWSXRay = require('./../services/tracer');
 const mysql = AWSXRay.captureMySQL(require('mysql'));
 
+// TODO: hook up with Amazon RDS
 const RDS_HOSTNAME = process.env.RDS_HOSTNAME || '';
 const RDS_USERNAME = process.env.RDS_USERNAME || '';
 const RDS_PASSWORD = process.env.RDS_PASSWORD || '';
 const RDS_PORT = process.env.RDS_PORT || '3306';
-const RDS_DB_NAME = process.env.RDS_DB_NAME || 'dbee';
+const RDS_DB_NAME = process.env.RDS_DB_NAME || '';
 
 var pool = mysql.createPool({
   host:     RDS_HOSTNAME,
