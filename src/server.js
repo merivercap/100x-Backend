4b332e8331f14a1d3ca9a7001d7833e134798ef1
niko79542 <nick.skoularikis@gmail.com>
2018-05-16 10:25:54 -0700

    Add dependencies for Koa

--- src/server.js
+++ src/server.js
@@ -1,48 +1,37 @@
-// process.env.NODE_ENV ? process.env.NODE_ENV : process.env.NODE_ENV = 'development';
-// const PORT = process.env.PORT || 3000;
+process.env.NODE_ENV ? process.env.NODE_ENV : process.env.NODE_ENV = 'development';
+const PORT = process.env.PORT || 3000;
 // const DEPLOYMENT_STAGE = process.env.DEPLOYMENT_STAGE || 'sandbox'
 const Koa = require('koa');
-// const errorhandler = require('errorhandler');
-// const db = require('./models/dao');
-// const routes = require('./controllers');
-// const fallback = require('express-history-api-fallback');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// const http = require('http');
+const Router = require('koa-router');
+const serve = require('koa-static');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
 const compress = require('koa-compress');
 const logger = require('koa-logger');
-// const serve = require('koa-static');
-// const route = require('koa-route');
 
-// Application setup.
+
+
 const app = new Koa();
+const router = new Router();
 
-// Logger
+
+
 app.use(logger());
 
-// app.use(route.get('/', books.home));
-// app.use(route.get('/books/', books.all));
-// app.use(route.get('/view/books/', books.list));
-// app.use(route.get('/books/:id', books.fetch));
-// app.use(route.post('/books/', books.add));
-// app.use(route.put('/books/:id', books.modify));
-// app.use(route.delete('/books/:id', books.remove));
-// app.use(route.options('/', books.options));
-// app.use(route.trace('/', books.trace));
-// app.use(route.head('/', books.head));
-
-// Connect all our routes to our application
-// app.use('/', routes);
 
 // Serve static files
-// app.use(serve(path.join(__dirname, 'public')));
-
-// Compress
+app.use(serve(path.join(__dirname, 'public')));
 app.use(compress());
 
+router
+  .get('/', (ctx, next) => {
+    ctx.body = 'hit steem api';
+    console.log('hitting api');
+  })
 
-app.use(async ctx => {
-    ctx.body = "It may work!\n";
-});
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
-app.listen(3000);
+app.listen(PORT);
