8791eb9cb864b757d164aece076934f9fa82dec2
niko79542 <nick.skoularikis@gmail.com>
2018-06-29 15:32:38 -0700

    Schedule a db update every 30 minutes

--- src/db/connectors.js
+++ src/db/connectors.js
@@ -1,12 +1,14 @@
 const casual = require('casual');
 const _ = require('lodash');
 
+const scheduler = require('node-schedule');
 const batchUpdate = require('../services/db_update');
 
 const db = require('./database_connection/tables');
 
 db.sequelize.sync().then(() => {
-  batchUpdate();
+
+  const j = scheduler.scheduleJob('*/30 * * * *', batchUpdate);
 });
 
 module.exports = db;
