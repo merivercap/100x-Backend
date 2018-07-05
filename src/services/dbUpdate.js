f7094361832400de8caafea37685b982feabb87e
Jay Hwang <jayhwang0121@gmail.com>
2018-07-05 00:04:20 -0700

    refactor(dbUpdate): refactor and add catch to promises

--- src/services/dbUpdate.js
+++ src/services/dbUpdate.js
@@ -1,48 +1,44 @@
+const logger = require('./logger');
 const {
   updatePostRanking,
   postExists,
   resetRanking,
   createPost,
-}                      = require('../db/models/dao');
-const client           = require('../server/steemAPI');
-// const taggings         = require('../utils/taggings');
-const taggings = [
-  'ethereum',
-];
+} = require('../db/models/dao');
+const client = require('../server/steemAPI');
+// const taggings = require('../utils/taggings');
+const taggings = [ 'ethereum' ];
+
 
 const batchUpdate = () => {
-  params = taggings.map((tag) => {
-    return [{"tag":tag,"limit":10}]
-  })
+  const params = taggings.map(tag => [{ "tag": tag, "limit": 10 }] );
   client.sendAsync('get_discussions_by_hot', params, handleResult);
 };
 
 const handleResult = (result) => {
-  resetRanking().then(() => {
-
-
-    for (let index = 0; index < result[0].length; index++) {
-      for (let tagIndex = 0; tagIndex < result.length; tagIndex++) {
-        let newHotRanking = index * 10 + tagIndex;
-        let post = result[tagIndex][index];
-        updateIfUnique(post, { newHotRanking });
+  resetRanking()
+    .then(() => {
+      for (let index = 0; index < result[0].length; index++) {
+        for (let tagIndex = 0; tagIndex < result.length; tagIndex++) {
+          let newHotRanking = index * 10 + tagIndex;
+          let post = result[tagIndex][index];
+          updateIfUnique(post, { newHotRanking });
+        }
       }
-    }
-  });
+    })
+    .catch(error => {
+      logger.error(`ERROR: `, error);
+    });
 }
 
-
-const updateIfUnique = (post, { newHotRanking }) => {
-  const postId = post.id;
-  return postExists(postId)
+const updateIfUnique = (post, { newHotRanking }) => (
+  postExists(post.id)
     .then(postIndeedExists => {
-      if (postIndeedExists) {
-        updatePostRanking({ postId, newHotRanking })
-      } else {
-        createPost(post, { newHotRanking });
-      }
-    });
-};
-
+      postIndeedExists ? updatePostRanking({ postId, newHotRanking }) : createPost(post, { newHotRanking });
+    })
+    .catch(error => {
+      logger.error(`ERROR: `, error);
+    })
+);
 
 module.exports = batchUpdate;
