32da0630fd65788b3262f0beb25aacaeff5e5b91
niko79542 <nick.skoularikis@gmail.com>
2018-07-19 14:02:54 -0700

    testUtils takes an int, which gets added to the id to create posts with different ID.  Seeding the random number generator seems like the solution here, but don't think Number#Random has a built in seeder

--- src/lib/testUtils.js
+++ src/lib/testUtils.js
@@ -10,8 +10,8 @@ module.exports = {
     return true;//sequelize.dropAllTables();
   },
 
-  createTestUserOpts: () => {
-    const randomNum = _.random(10000);
+  createTestUserOpts: (number = 0) => {
+    const randomNum = _.random(10000) + number;
     const name = `name${randomNum}`;
 
     return {
@@ -20,23 +20,23 @@ module.exports = {
     };
   },
 
-  createTestReplyOpts: () => {
-    const randomNum = _.random(10000);
+  createTestReplyOpts: (number = 0) => {
+    const randomNum = _.random(10000) + number;
     const body = `
       We have a relational database working behind, so do its principles apply also for Sequelize which is a ORM for relational databases. The easiest would be to create another table or entity and associate them as a 1:n relationship. For that matter add a new model in Sequelize and define its associations like described here: http://sequelizejs.com/articles/getting-started#associations You might have then 2 tables. One profile table having N pictures.
     `;
-    
+
     return {
       id: randomNum,
-      permLink: 're-kingscrown-get-byteballs-free-and-help-someone-you-follow-or-like-to-make-money-too-20180719t171429006z',
+      permLink: `${randomNum}-re-kingscrown-get-byteballs-free-and-help-someone-you-follow-or-like-to-make-money-too-20180719t171429006z`,
       body,
       netVotes: 1,
       pendingPayoutValue: 33.33,
     }
   },
 
-  createTestPostOpts: () => {
-    const randomNum = _.random(10000);
+  createTestPostOpts: (number = 0) => {
+    const randomNum = _.random(10000) + number;
     const title = `Test post title ${randomNum}`;
     const body = `
       We have a relational database working behind, so do its principles apply also for Sequelize which is a ORM for relational databases. The easiest would be to create another table or entity and associate them as a 1:n relationship. For that matter add a new model in Sequelize and define its associations like described here: http://sequelizejs.com/articles/getting-started#associations You might have then 2 tables. One profile table having N pictures.
@@ -44,7 +44,7 @@ module.exports = {
 
     return {
       id: randomNum,
-      permLink: 'first-post',
+      permLink: `${randomNum}first-post`,
       title,
       body,
       netVotes: 1,
