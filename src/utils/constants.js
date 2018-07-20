6c335537e5719427e897754e3443a636269e0283
niko79542 <nick.skoularikis@gmail.com>
2018-07-20 14:52:54 -0700

    add replyService which fetches replies for a given post.   Nested replies require additional calls to steemits api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
-module.exports.FETCH_POSTS_PER_TAG = 2;
+module.exports.FETCH_POSTS_PER_TAG = 1;
 module.exports.HOT = 'hot';
 module.exports.TRENDING = 'trending';
 module.exports.GET_DISCUSSIONS_BY_HOT = 'get_discussions_by_hot';
 module.exports.GET_DISCUSSIONS_BY_TRENDING = 'get_discussions_by_trending';
+module.exports.GET_CONTENT_REPLIES = 'get_content_replies';
