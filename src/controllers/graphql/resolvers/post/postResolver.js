d47b8ff8d1dbbf72b9352f94a5f91419d3b21291
niko79542 <nick.skoularikis@gmail.com>
2018-07-23 10:03:34 -0700

    Get Follower Posts

--- src/controllers/graphql/resolvers/post/postResolver.js
+++ src/controllers/graphql/resolvers/post/postResolver.js
@@ -6,12 +6,17 @@ const ReplyService = require('../../../../services/replyService');
 
 module.exports = {
   Query: {
-    getAllPosts: async (_, args, { user }) => {
+    getAllPosts: async (_, args) => {
       return await Post.findAll({order: [['hot', 'ASC']], limit: 100});
     },
     getPost: async (_,args) => {
       return await Post.findById(args.postId);
     },
+    getFollowerPosts: async (_, args, { authenticatedUserInstance }) => {
+      return !authenticatedUserInstance
+        ? new AuthenticationError('ERROR_GETTING_FOLLOWER_POSTS')
+        : await authenticatedUserInstance.getUsersFollowerPosts();
+    }
   },
   Post: {
     author(post) {
