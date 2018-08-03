a9cfaaae84f4dc5fc076bbb9711a286fdf2fc79b
niko79542 <nick.skoularikis@gmail.com>
2018-08-03 11:40:23 -0700

    refactor postService

--- src/services/postService.js
+++ src/services/postService.js
@@ -13,42 +13,35 @@ const {
   FETCH_TOP_X_POSTS,
   HUNDREDX_USERNAME,
   GET_BLOG_ENTRIES,
+  GET_CONTENT,
 }                  = require('../utils/constants');
 const VIDEO_URLS   = require('../utils/videoUrls');
 
 module.exports = {
   reSyncPosts: function(posts, rankType) {
     for (const [tagIndex, postsByTag] of Object.entries(posts)) {
-      for (const [index, post] of Object.entries(postsByTag)) {
-        const newRanking = (tagIndex) * FETCH_POSTS_PER_TAG + parseInt(index);
-        const updateRankType = {};
-        updateRankType[rankType] = newRanking;
+      for (const [postIndex, steemitPost] of Object.entries(postsByTag)) {
+        // for each post from Steemit API...
+        const newRanking = this.calculateNewRank(tagIndex, postIndex);
+        const newRankingObj = this.generateProprietaryRankObject(rankType, newRanking);
+        const postForOurDb = this.formatSteemitPost(steemitPost);
+
         UserModel
           .findOrCreate({
-            where: {name: post.author},
-            defaults: { id: _.random(10000)}
+            where: {id: steemitPost.author},
           })
-          .spread((user, created) => {
-            return this.findOrCreatePost(post, user, updateRankType);
-          });
+          .spread((userRecord, created) => {
+            return this.findOrCreatePost({...postForOurDb, ...newRankingObj}, userRecord);
+          })
+          .catch(err => console.log("trouble finding or creating post author", err));
 
       }
     }
   },
 
-  resetRanking: function(rankType) {
-     //updates all posts of rankType, since children is always greater than 0
-     const keyVal = {};
-     keyVal[rankType] = 9999;
-    return PostModel.update(keyVal, {
-      where: {children: {[Op.gte]: 0} }
-    })
-      .catch(err => console.log(err));
-  },
-
   getPostsOfAuthors: function(authors) {
     const authorObjs = authors.map(name => {
-      return { name };
+      return { id: name };
     });
     return PostModel.findAll({
       include: [
@@ -60,6 +53,25 @@ module.exports = {
          }
       ],
     });
+  },
+
+ fetchSingleSteemitPost: function(permlink, userInOurDb) {
+   const storeSteemitPostInOurDb = (post) => {
+     const postForOurDb = this.formatSteemitPost(post[0]);
+     return this.findOrCreatePost(postForOurDb, userInOurDb);
+    }
+    const params = [[userInOurDb.name, permlink]];
+    return client.sendAsync(GET_CONTENT, params, storeSteemitPostInOurDb);
+  },
+
+  findOrCreatePost: function(postObj, author) {
+    return PostModel
+      .findOrCreate({
+        where: {id: postObj.id},
+        defaults: { ...postObj, userId: author.id }
+      }).spread((postInOurDb, postWasCreated) => {
+        return postInOurDb;
+      }).catch(err => console.log(err));
   },
 
   fetchHundredxResteemedPosts: async function() {
@@ -89,6 +101,7 @@ module.exports = {
        return NEWS_POST;
      }
   },
+
   linkContainsVideoUrl: function(link) {
     for (const videoUrl of VIDEO_URLS) {
       if (link.includes(videoUrl)) {
@@ -96,6 +109,7 @@ module.exports = {
       }
     }
   },
+
   containsVideo: function(links) {
     for (let link of links) {
       if (this.linkContainsVideoUrl(link)) {
@@ -104,18 +118,21 @@ module.exports = {
     }
       return false;
   },
-  postProperFormat: function(post) {
-    const metadata = JSON.parse(post.json_metadata);
-    const convertedValue = Number.parseFloat(post.pending_payout_value.split("SBD")[0]);
+
+  formatSteemitPost: function(steemitPost) {
+    const metadata = JSON.parse(steemitPost.json_metadata);
+    const convertedValue = Number.parseFloat(steemitPost.pending_payout_value.split("SBD")[0]);
+
     const tags = metadata.tags;
     const links = metadata.links;
     return {
-      permLink: post.permlink,
-      title: post.title,
-      body: post.body,
-      createdAt: post.created,
-      netVotes: post.net_votes,
-      children: post.children,
+      id: steemitPost.id,
+      permLink: steemitPost.permlink,
+      title: steemitPost.title,
+      body: steemitPost.body,
+      createdAt: steemitPost.created,
+      netVotes: steemitPost.net_votes,
+      children: steemitPost.children,
       pendingPayoutValue: convertedValue,
       postType: this.determinePostType(links),
       tag1: tags[0],
@@ -125,21 +142,7 @@ module.exports = {
       tag5: tags[5],
     }
   },
-  findOrCreatePost: function(post, author, updateRankType) {
-    return PostModel
-      .findOrCreate({
-        where: {id: post.id},
-        defaults: { ...this.postProperFormat(post), userId: author.id }
-      }).spread((post, created) => {
-        if (created) {
-          const replyFetcher = new ReplyService(post);
-          return post.update(updateRankType)
-            // .then(replyFetcher.getAllComments());
-        } else {
-          return post.update(updateRankType);
-        }
-      });
-  },
+
   extractFromSteemitResponse: function(posts) {
     return posts.map(post => (
       { permLink: post.permlink, name: post.author }
@@ -147,13 +150,33 @@ module.exports = {
   },
   findByPermLinkAndAuthor: function(permLink, name) {
     return PostModel.findOne({
-      where: { permLink },
+      where: { permLink }, //might work with userId is name....?
       include: [
         {
           model: UserModel,
           where: { name }
         }
       ]
-    });
+    }).catch(err => console.log("trouble finding existing post", err));
+  },
+
+  calculateNewRank: function(tagRank, postRank) {
+    return tagRank * FETCH_POSTS_PER_TAG + parseInt(postRank);
+  },
+
+  generateProprietaryRankObject: function(rankType, newRanking) {
+    const obj = {};
+    obj[rankType] = newRanking;
+    return obj
+  },
+
+  resetRanking: function(rankType) {
+     //updates all posts of rankType, since children is always greater than 0
+     const keyVal = {};
+     keyVal[rankType] = 9999;
+    return PostModel.update(keyVal, {
+      where: {children: {[Op.gte]: 0} }
+    })
+      .catch(err => console.log(err));
   },
 }
