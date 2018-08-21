1df09210a2232123cc09ea3921fbef174fcbb068
Jay Hwang <jayhwang0121@gmail.com>
2018-08-20 20:17:57 -0700

    feat/refactor(replyService.js): minor refactor in reply Service

--- src/services/replyService.js
+++ src/services/replyService.js
@@ -24,51 +24,49 @@ class ReplyService {
         this.postAuthor = author;
         return this.fetchRepliesFromSteemit({
           usersSteemitName: this.postAuthor.name,
-          permLink: this.post.permLink,
-        })
+          permLink: this.post.permLink
+        });
       })
       .catch(err => console.log("trouble adding replies to db: ", err));
   }
   // user and permlink can be info of the Steemit POST OR COMMENT
   fetchRepliesFromSteemit({ usersSteemitName, permLink }) {
-    const addRepliesToDb = (replies) => {
-      for (const reply of replies[0]) { // replies returned in 2-D array.  Our client.sendAsync supports multiple requests..
+    const addRepliesToDb = replies => {
+      for (const reply of replies[0]) {
+        // replies returned in 2-D array.  Our client.sendAsync supports multiple requests..
         this.addReplyToDb(reply);
       }
-    }
-    const params = [[ usersSteemitName, permLink]];
+    };
+    const params = [[usersSteemitName, permLink]];
     return client.sendAsync(GET_CONTENT_REPLIES, params, addRepliesToDb);
   }
 
-  addReplyToDb(steemitReply) {
-    UserModel
-      .findOrCreate({
-        where: {name: steemitReply.author},
-        defaults: { id: _.random(10000) }
-      })
-      .spread((commenter, created) => {
-        return this.findOrCreateReply(steemitReply, commenter);
-      });
+  findOrCreateReply(steemitReply, commenter) {
+    return ReplyModel.findOrCreate({
+      where: { id: steemitReply.id },
+      defaults: {
+        ...this.replyProperFormat(steemitReply),
+        userId: commenter.id
+      }
+    }).spread((replyInOurDb, created) => {
+      replyInOurDb.update({ body: steemitReply.body });
+      // NestedReplies require seperate calls to Steemit Api...
+      // .then(replyInOurDb => {
+      //   if (replyInOurDb.children && created) {
+      //     return this.fetchRepliesFromSteemit({ usersSteemitName: steemitReply.name, permLink: steemitReply.permlink })
+      //   }
+      // });
+    });
   }
 
-  findOrCreateReply(steemitReply, commenter) {
-    return ReplyModel
-      .findOrCreate({
-        where: {id: steemitReply.id},
-        defaults: { ...this.replyProperFormat(steemitReply), userId: commenter.id }
-      })
-      .spread((replyInOurDb, created) => {
-        replyInOurDb.update({body: steemitReply.body})
-          // NestedReplies require seperate calls to Steemit Api...
-          // .then(replyInOurDb => {
-          //   if (replyInOurDb.children && created) {
-          //     return this.fetchRepliesFromSteemit({ usersSteemitName: steemitReply.name, permLink: steemitReply.permlink })
-          //   }
-          // });
-      });
+  getAllRepliesByPostId(postId) {
+    return ReplyModel.findAll({ where: { postId } });
   }
+
   replyProperFormat(steemitReply) {
-    const convertedValue = Number.parseFloat(steemitReply.pending_payout_value.split("SBD")[0]);
+    const convertedValue = Number.parseFloat(
+      steemitReply.pending_payout_value.split("SBD")[0]
+    );
     return {
       postId: this.post.id,
       permLink: steemitReply.permlink,
@@ -77,8 +75,8 @@ class ReplyService {
       netVotes: steemitReply.net_votes,
       pendingPayoutValue: convertedValue,
       depth: steemitReply.depth,
-      children: steemitReply.children,
-    }
+      children: steemitReply.children
+    };
   }
 }
 
