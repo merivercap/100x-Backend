393eaaafaab0dea3300eb50c7736dbbfe3421fe5
niko79542 <nick.skoularikis@gmail.com>
2018-10-11 23:14:05 -0700

    change constants bac

--- src/controllers/graphql/connectors/connectors.js
+++ src/controllers/graphql/connectors/connectors.js
@@ -4,8 +4,8 @@ const scheduler = require('node-schedule');
 const db = require('../../../models/sequelize');
 
 db.sequelize.sync({force: false}).then(() => {
-  reCalibrateHotAndTrendingPosts();
-  // const j = scheduler.scheduleJob('*/30 * * * *', reCalibrateHotAndTrendingPosts);
+  // reCalibrateHotAndTrendingPosts();
+  const j = scheduler.scheduleJob('*/30 * * * *', reCalibrateHotAndTrendingPosts);
 });
 
 module.exports = db;
